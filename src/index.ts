da38542c8f1bf4f6ece7cb0fd77833a524770848
PatrickNiyogitare28 <patrickniyogitare28@gmail.com>
2021-06-28 23:39:35 +0200

    (chore): added endpoints

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import {User} from './Entities/User';
 
 const app:Application = express();
 const {PORT, DEV_DB_NAME, DEV_DB_USERNAME, DEV_DB_PASSWORD} = process.env;
+const BASE_URL = '/api/v1'
 createConnection({
     type:'mysql', //db type
     database: DEV_DB_NAME, //database name
@@ -16,10 +17,34 @@ createConnection({
 
 })
 
+app.use(express.urlencoded({extended:true}));
+
 app.get("/", (_:Request,res:Response, next:NextFunction) => {
     res.send("Hello");
 })
 
+app.get(`${BASE_URL}/users`, (_:Request, res:Response) => {
+    User.find().then((data:[]) => {
+        res.json(data);
+    })
+    .catch((error:any) => {
+        res.json(error)
+    })
+})
+/*
+  @Todo debug reqbody
+*/
+app.post(`${BASE_URL}/users`, async(req:Request, res:Response) => {
+ let user;
+  try{
+    user = await User.insert(req.body);
+  }
+  catch(e){
+     res.status(400).json(e);
+  }
+
+    res.status(201).json(user);
+});
 
 app.listen(PORT, ():void => {
     console.log("Server 🏃‍♂️🏃‍♂️🏃‍♂️🏃‍♂️ "+PORT)
