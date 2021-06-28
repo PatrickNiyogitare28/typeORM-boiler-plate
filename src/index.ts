870b0f3ae3b5ebcbb02be364b7f516ff2db03712
PatrickNiyogitare28 <patrickniyogitare28@gmail.com>
2021-06-28 22:28:39 +0200

    (chore):added custome types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express,{Application, Request, Response, NextFunction} from "express";
 
-const app = express();
+const app:Application = express();
 
-app.get("/", (_,res) => {
+app.get("/", (_:Request,res:Response, next:NextFunction) => {
     res.send("Hello");
 })
 
